11ecc40cb1487a13c15c6862a7ce8d445eca7d0d
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-07-02 21:53:24 -0400

    Generate migrations for posts user_id & add authorization functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_165417) do
+ActiveRecord::Schema.define(version: 2021_07_02_205850) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
